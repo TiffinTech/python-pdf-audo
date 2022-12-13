3b706e329f91b282625da6147348ee1bf91274a6
TiffinTech <99835088+TiffinTech@users.noreply.github.com>
2022-12-13 14:08:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyttsx3,PyPDF2
 
+#insert name of your pdf 
 pdfreader = PyPDF2.PdfFileReader(open('book.pdf', 'rb'))
 speaker = pyttsx3.init()
 
@@ -7,7 +8,7 @@ for page_num in range(pdfreader.numPages):
     text = pdfreader.getPage(page_num).extractText()
     clean_text = text.strip().replace('\n', ' ')
     print(clean_text)
-
+#name mp3 file whatever you would like
 speaker.save_to_file(clean_text, 'story.mp3')
 speaker.runAndWait()
 

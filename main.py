d69c41105bc77ecb45a1875e8a0f6b52da8573ee
SaiTeja-002 <95877599+SaiTeja-002@users.noreply.github.com>
2023-01-03 18:21:20 +0530

    Fixed the text overwriting issue

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pyttsx3,PyPDF2
 
 #insert name of your pdf 
-pdfreader = PyPDF2.PdfFileReader(open('book.pdf', 'rb'))
+pdfreader = PyPDF2.PdfFileReader(open("book.pdf", 'rb'))
 speaker = pyttsx3.init()
 
+text = ""
 for page_num in range(pdfreader.numPages):
-    text = pdfreader.getPage(page_num).extractText()
-    clean_text = text.strip().replace('\n', ' ')
-    print(clean_text)
+    text += pdfreader.getPage(page_num).extractText()
+
+clean_text = text.strip().replace('\n', ' ')
+print(clean_text)
+
 #name mp3 file whatever you would like
-speaker.save_to_file(clean_text, 'story.mp3')
+speaker.save_to_file(clean_text, "story.mp3")
 speaker.runAndWait()
 
 speaker.stop()

c5d9e9de217111ee3629fc1c400ea8ad72f3940c
Patrick-Emil Zörner <paddyez@users.noreply.github.com>
2023-01-08 10:35:11 +0100

    One liner for loop

--- main.py
+++ main.py
@@ -14,15 +14,10 @@ def speak_and_save_audio(read_pdf):
 
 
 def read_pdf():
-    clean_text = ''
     pdf = PdfReader('book.pdf')
-    for page_num in range(len(pdf.pages)):
-        page = pdf.pages[page_num]
-        text = re.sub('\s+', ' ', page.extract_text().strip())
-        clean_text = clean_text + text
-    return clean_text
+    return ' '.join([re.sub('\s+', ' ', page.extract_text().strip()) for page in pdf.pages])
 
 
 if __name__ == '__main__':
-    #print(read_pdf())
+    print(read_pdf())
     speak_and_save_audio(read_pdf())

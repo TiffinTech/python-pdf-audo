fe771fc9f6384b9c06fcbb70a0217fe763827154
kidexx <kidex@gmx.de>
2023-11-09 08:24:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pyttsx3,PyPDF2
 
 #insert name of your pdf 
-pdfreader = PyPDF2.PdfFileReader(open('book.pdf', 'rb'))
+pdfreader = PyPDF2.PdfReader(open('book2.pdf', 'rb'))
 speaker = pyttsx3.init()
 
-for page_num in range(pdfreader.numPages):
-    text = pdfreader.getPage(page_num).extractText()
+for page_num in range(len(pdfreader.pages)):
+    text = pdfreader.pages[page_num].extract_text()
     clean_text = text.strip().replace('\n', ' ')
     print(clean_text)
 #name mp3 file whatever you would like
-speaker.save_to_file(clean_text, 'story.mp3')
+speaker.save_to_file(clean_text, 'story2.mp3')
 speaker.runAndWait()
 
 speaker.stop()
